9d7691ad86d83d0f6cc3a7d0dca1d8779b67cc0e
aracloud869 <congdanh123k@gmail.com>
2026-08-22 03:25:21 +0700

    Okkkk

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,9 +2,18 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
-import firebaseConfig from "../firebase-applet-config.json";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA2884RH2sOARsRAqChEQ0XzFS5VRXIFJQ",
+  authDomain: "cineflix-aaa32.firebaseapp.com",
+  databaseURL: "https://cineflix-aaa32-default-rtdb.firebaseio.com",
+  projectId: "cineflix-aaa32",
+  storageBucket: "cineflix-aaa32.firebasestorage.app",
+  messagingSenderId: "1079426419467",
+  appId: "1:1079426419467:web:caeaf7ab3f3edeb4a8b00e"
+};
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
+export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
